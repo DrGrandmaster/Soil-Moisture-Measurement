9d7809298937372d4d49b69889e62835a77ea8be
DrGrandmaster <46546882+DrGrandmaster@users.noreply.github.com>
2023-04-08 20:03:59 -0600

    added temp sensing

--- pico_code/main.py
+++ pico_code/main.py
@@ -8,6 +8,9 @@ pHtime = Timer()
 moistRead = ADC(1)
 moistTime = Timer()
 
+tempRead = ADC(4)
+tempTime = Timer()
+
 #corrects to voltage on adc pin
 factorADC = 3.3 / (65535)
 
@@ -21,6 +24,10 @@ def readPH(pHtimer):
 def readMoist(moistTime):
     print(moistRead.read_u16() * factorADC * factorMoist)
 
+def readTemp(tempTime):
+    print(27 - ((tempRead.read_u16() * factorADC) - 0.706)/0.001721)
+
 
 pHtime.init(freq= 20, mode=Timer.PERIODIC, callback=readPH) 
 moistTime.init(freq= 20, mode=Timer.PERIODIC, callback=readMoist)
+tempTime.init(freq= 20, mode=Timer.PERIODIC, callback=readTemp)
