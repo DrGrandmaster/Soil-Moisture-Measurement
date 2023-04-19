96c90ae16158ca16d9d27f772a25ede57cdf4821
CARR787 <129337634+CARR787@users.noreply.github.com>
2023-04-18 21:58:26 -0600

    Update main.py

--- pico_code/main.py
+++ pico_code/main.py
@@ -24,8 +24,8 @@ factorPH = 1.0
 factorMoist = 1.0
 
 def readPH(pHtimer):
-    print(phRead.read_u16() * factorADC * factorPH)
-
+    voltage = (phRead.read_u16() * factorADC * factorPH)
+    print(0.5*(voltage-1.1) + 7.0) #The numbers are temporary
 def readMoist(moistTime):
     print(moistRead.read_u16() * factorADC * factorMoist)
 
@@ -36,3 +36,64 @@ def readTemp(tempTime):
 pHtime.init(freq= 20, mode=Timer.PERIODIC, callback=readPH) 
 moistTime.init(freq= 20, mode=Timer.PERIODIC, callback=readMoist)
 tempTime.init(freq= 20, mode=Timer.PERIODIC, callback=readTemp)
+
+ssid = 'Design_Wifi'
+password = 'Water123'
+
+def connect():
+    #Connect to WLAN
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    wlan.connect(ssid, password)
+    while wlan.isconnected() == False:
+        print('Waiting for connection...')
+        sleep(1)
+    print(wlan.ifconfig())
+    ip = wlan.ifconfig()[0]
+    print(f'Connected on {ip}')
+    return ip
+
+###
+    
+def open_socket(ip):
+    # Open a socket
+    address = (ip, 80)
+    connection = socket.socket()
+    connection.bind(address)
+    connection.listen(1)
+    return connection
+
+###
+def webpage(temperature): #ignore
+    #Template HTML
+    html = f"""
+            <!DOCTYPE html>
+            <html>
+            <p>Temperature is {temperature}</p>
+            </body>
+            </html>
+            """
+    return str(html)
+###
+def serve(connection): #ignore
+    #Start a web server
+    pico_led.off()
+    temperature = 0
+    while True:
+        client = connection.accept()[0]
+        request = client.recv(1024)
+        request = str(request)
+        try:
+            request = request.split()[1]
+        except IndexError:
+            pass
+        temperature = pico_temp_sensor.temp
+        html = webpage(temperature)
+        client.send(html)
+        client.close()
+
+try:
+    ip = connect()
+    connection = open_socket(ip)
+except KeyboardInterrupt:
+    machine.reset()
